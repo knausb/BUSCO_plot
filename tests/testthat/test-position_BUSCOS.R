2ba456c0fc8510424ca36c003fb7bc7e9f1b4a15
knausb <briank.lists@gmail.com>
2023-12-18 11:41:11 -0800

    used usethis::use_testthat()

---
+++ tests/testthat/test-position_BUSCOS.R
@@ -0,0 +1,26 @@
+
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
+
+
+test_that("position_buscos works", {
+  DM6file <- system.file("extdata", "DM6_full_table.tsv.gz", package = 'BUSCOplot')
+  DM6 <- read_busco(DM6file)
+  chromL <- make_known(DM6, sample_name = "DM6:")
+  #lapply(chromL[1:2], head, n=2)
+  ATLfile <- system.file("extdata", "atlantic_full_table.tsv.gz", package = 'BUSCOplot')
+  ATL <- read_busco(ATLfile)
+  chromL <- add_unknown(known_list = chromL, unknown_df = ATL, sample_name = "ATL:")
+  #lapply(chromL[1:2], head, n=2)
+ 
+  #gg_line_map(chromL[[1]])
+  tmp <- position_BUSCOS(chromL[[1]])
+  
+  expect_equal(ncol(tmp), 14)
+  expect_equal(nrow(tmp), 329)
+})
+ 
+ 
+ 
+ 
