fe6b5b659f3a55b1e9e4f1de594d3d972e2c2a0a
knausb <briank.lists@gmail.com>
2024-05-17 07:24:22 -0700

    added header processing to read_busco()

--- R/read_busco.R
+++ R/read_busco.R
@@ -11,9 +11,16 @@
 #' 
 #' @export
 read_busco <- function(file){
+  my_header <- scan( file = file, what = "character", n = 10, sep = "\n", quiet = TRUE)
+  my_header <- my_header[ grep("^# Busco id", my_header) ]
+  my_header <- strsplit(my_header, split = "\t")
+  my_header <- unlist(my_header)
+  my_header <- sub("^# ", "", my_header)
+
   busc <- utils::read.table(file, header = FALSE, sep = "\t", fill = TRUE)
-  names(busc) <- c("Busco_id", "Status", "Sequence", "Gene_Start",
-                   "Gene_End", "Strand", "Score", "Length")
+  # names(busc) <- c("Busco_id", "Status", "Sequence", "Gene_Start",
+  #                  "Gene_End", "Strand", "Score", "Length")
+  names(busc) <- my_header
   busc$Sequence[busc$Sequence == ""] <- "Unplaced"
   return(busc)
 }
